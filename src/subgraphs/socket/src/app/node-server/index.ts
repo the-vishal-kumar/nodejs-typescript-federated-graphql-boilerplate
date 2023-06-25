6deb0c570d771042424583d744f41f3679d25170
Vishal Kumar <vishal194kumar@gmail.com>
2023-06-25 20:11:48 +0200

    chore: socket resolver refactored

--- src/subgraphs/socket/src/app/node-server/index.ts
+++ src/subgraphs/socket/src/app/node-server/index.ts
@@ -3,68 +3,16 @@ import { join } from 'node:path';
 import { ApolloServer, gql } from 'apollo-server';
 import { buildSubgraphSchema } from '@apollo/subgraph';
 import { GraphQLError } from 'graphql';
-import { Logger, getEnumKey } from '../../util';
-import { Query } from '../../resolver';
-import {
-  IConnection,
-  IDataProvider,
-  IDataProviderStatusTypeTitle,
-  IDataProviderTitle,
-  ISocketDocument,
-  IFormalName,
-  ICurrentTypeTitle,
-  IDescription,
-  IComments,
-  ILevelTitle,
-} from '../../type';
+import { Logger } from '../../util';
+import { resolvers } from '../../resolver';
 
 const schemaPath = join(__dirname, 'schema.graphql');
 
 const buildSocketSubgraph = async (port: number): Promise<void> => {
   const gqlContent = readFileSync(schemaPath, { encoding: 'utf-8' });
   const typeDefs = gql(gqlContent);
-  const resolvers = {
-    Query: {
-      socket: Query.socket,
-      sockets: Query.sockets,
-    },
-    Socket: {
-      identifier: ({ _id }: ISocketDocument): string => _id,
-      dataProvider: ({ dataProvider }: ISocketDocument): IDataProvider => ({
-        ...dataProvider,
-        dataProviderStatusType: {
-          ...dataProvider.dataProviderStatusType,
-          title: getEnumKey(
-            IDataProviderStatusTypeTitle,
-            dataProvider.dataProviderStatusType.title,
-          ) as IDataProviderStatusTypeTitle,
-        },
-        title: getEnumKey(IDataProviderTitle, dataProvider.title) as IDataProviderTitle,
-      }),
-      connections: ({ connections }: ISocketDocument): IConnection[] => {
-        const connectionSubDocs = JSON.parse(JSON.stringify(connections));
-        return connectionSubDocs.map((connection: IConnection) => ({
-          ...connection,
-          connectionType: {
-            ...connection.connectionType,
-            formalName: getEnumKey(IFormalName, connection.connectionType.formalName) as IFormalName,
-          },
-          currentType: {
-            ...connection.currentType,
-            description: getEnumKey(IDescription, connection.currentType?.description),
-            title: getEnumKey(ICurrentTypeTitle, connection.currentType?.title),
-          },
-          level: {
-            ...connection.level,
-            comments: getEnumKey(IComments, connection.level?.comments),
-            title: getEnumKey(ILevelTitle, connection.level?.title),
-          },
-        }));
-      },
-    },
-  };
-
   const schema = buildSubgraphSchema({ typeDefs, resolvers });
+
   const server = new ApolloServer({
     schema,
     introspection: process.env.ENVIRONMENT !== 'production',
--- src/subgraphs/socket/src/resolver/index.ts
+++ src/subgraphs/socket/src/resolver/index.ts
@@ -1,3 +1,56 @@
 import * as Query from './query';
+import { getEnumKey } from '../util';
+import {
+  ISocketDocument,
+  IDataProvider,
+  IDataProviderStatusTypeTitle,
+  IDataProviderTitle,
+  IConnection,
+  IFormalName,
+  IDescription,
+  ICurrentTypeTitle,
+  IComments,
+  ILevelTitle,
+} from '../type';
 
-export { Query };
+export const resolvers = {
+  Query: {
+    socket: Query.socket,
+    sockets: Query.sockets,
+  },
+  Socket: {
+    identifier: ({ _id }: ISocketDocument): string => _id,
+    // id: ({ serial }: ISocketDocument): number => serial,
+    dataProvider: ({ dataProvider }: ISocketDocument): IDataProvider => ({
+      ...dataProvider,
+      dataProviderStatusType: {
+        ...dataProvider.dataProviderStatusType,
+        title: getEnumKey(
+          IDataProviderStatusTypeTitle,
+          dataProvider.dataProviderStatusType.title,
+        ) as IDataProviderStatusTypeTitle,
+      },
+      title: getEnumKey(IDataProviderTitle, dataProvider.title) as IDataProviderTitle,
+    }),
+    connections: ({ connections }: ISocketDocument): IConnection[] => {
+      const connectionSubDocs = JSON.parse(JSON.stringify(connections));
+      return connectionSubDocs.map((connection: IConnection) => ({
+        ...connection,
+        connectionType: {
+          ...connection.connectionType,
+          formalName: getEnumKey(IFormalName, connection.connectionType.formalName) as IFormalName,
+        },
+        currentType: {
+          ...connection.currentType,
+          description: getEnumKey(IDescription, connection.currentType?.description),
+          title: getEnumKey(ICurrentTypeTitle, connection.currentType?.title),
+        },
+        level: {
+          ...connection.level,
+          comments: getEnumKey(IComments, connection.level?.comments),
+          title: getEnumKey(ILevelTitle, connection.level?.title),
+        },
+      }));
+    },
+  },
+};
